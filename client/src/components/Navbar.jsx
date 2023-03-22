f36829cbd96f0fdc1449359c6afd41f27717f73e
Johanz Pineda <johanzpineda@gmail.com>
2023-03-22 19:47:03 +0800

    feat: Navbar added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../images/logo-new.png'
 import './Navbar.css'
 
 export default function Navbar() {
+
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+  
   return (
   <>
-  <h1> Helloww</h1>
+  <div className='navbar'>
+    <div className='logo'>
+      <img src={Logo} alt='logo' />
+    </div>
+    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+      <li className='nav-item'><a href='/'>Home</a></li>
+      <li className='nav-item'><a href='/'>About</a></li>
+      <li className='nav-item'><a href='/'>FAQs</a></li>
+      <li className='nav-item'><a href='/'>Contact</a></li>
+    </ul>
+    <div className='hamburger' onClick={handleClick}>
+      {click ? (<FaTimes size={30} style={{color: '#f8f8f8'}} />) : (<FaBars size={30} style={{color: '#f8f8f8'}} />) }
+      
+    </div>
+  </div>
   </>
     
   )
